d7cdf6713176062183bf0108f96543239a221367
mrat <78581772+mr4tt@users.noreply.github.com>
2024-09-07 04:29:26 -0700

    feat: adding HTTP routes

--- main.go
+++ main.go
@@ -13,26 +13,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// response: https://teller.io/docs/api/identity#get-identity
-// links: https://teller.io/docs/api/accounts#properties
-type CapitalOneResp struct {
-	Type         string `json:"type"`
-	Subtype      string `json:"subtype"`
-	Status       string `json:"status"`
-	Name         string `json:"name"`
-	Links        struct {
-		Transactions string `json:"transactions"`
-		Self         string `json:"self"`
-		Balances     string `json:"balances"`
-	} `json:"links"`
-	Institution struct {
-		Name         string `json:"name"`
-		ID           string `json:"id"`
-	} `json:"institution"`
-	ID           string `json:"id"`
-	EnrollmentID string `json:"enrollment_id"`
-}
-
 // make a GET request, given auth, url to request, and a client (for certs)
 func getReq(url string, accessToken string, client *http.Client) []byte {
 	request, err := http.NewRequest("GET", url, nil)
@@ -60,39 +40,56 @@ func getReq(url string, accessToken string, client *http.Client) []byte {
 	return fullResponse
 }
 
-// get account info from TC (teller connect) code, then use to get transactions
-func getTransactions(accessToken string, client *http.Client) [][]byte {
+// get account info from a TC (teller connect) code, then use to get transactions
+func getTransactions(accessToken string, client *http.Client) []Transaction {
 	url := "https://api.teller.io/accounts"
 
 	accounts := getReq(url, accessToken, client) 
 
 	var accInfo []CapitalOneResp
 
+	// convert response from list of json into list of CapitalOneResp type
 	err := json.Unmarshal([]byte(accounts), &accInfo) 
 	if err != nil {
 		fmt.Println("Error unmarshalling:", err)
 		return nil
 	}
-	var transactions [][]byte 
+
+	var transactions []Transaction 
+
+	// subtypes of accounts are
+	// depository:
+	// checking, savings, money_market, certificate_of_deposit, treasury, sweep
+	// credit:
+	// credit_card
+
+	// maybe we should ignore everything except checking and credit card 
+
+	// for each account found, get the transactions from it and 
+	// convert to Transaction type
 	for _, account := range accInfo {
 		fmt.Println("ID:", account.ID)
 		fmt.Println("Name:", account.Name)
 
-		// should probably filter on if its a credit card or not 
-		// actually ed wants checking for rent tracking 
-		// actually kale uses his debit card smh
-		transactions = append(transactions, getReq(account.Links.Transactions, accessToken, client))
+		accTransaction := getReq(account.Links.Transactions, accessToken, client)
+		err = json.Unmarshal((accTransaction), &transactions) 
+		if err != nil {
+			fmt.Println("Error unmarshalling transactions:", err)
+			return nil
+		}
 	}
 
 	return transactions
 }
+
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
+	// load secets from .env
+	if err := godotenv.Load(".env"); err != nil {
 		fmt.Println("Error loading .env:", err)
 		return
 	}
 	
+	// set up auth to Teller API (SSL certs and access token)
 	certFile := "certs/certificate.pem"
 	keyFile := "certs/private_key.pem"
 	accessToken := os.Getenv("ACCESS_TOKEN")
@@ -128,12 +125,37 @@ func main() {
 	transactions := getTransactions(accessToken, client)
 
 	r.Get("/transactions", func(w http.ResponseWriter, r *http.Request) {
-		for _, transaction := range transactions {
-			w.Write(transaction)
+		if err := json.NewEncoder(w).Encode(transactions); err != nil {
+			http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+			return
 		}
 	})
+
+	// mounts paths from Routes() and needs them to start with /api
+	r.Mount("/api", Routes())
 
 	fmt.Println("http://localhost:3000/transactions") 	
 	http.ListenAndServe(":3000", r)
 	
+}
+
+func Routes() chi.Router {
+    r := chi.NewRouter()
+
+    handler := Handler{}
+
+	// to use this, go to localhost:3000/api/{id}/moneyLeft
+    r.Get("/{id}/moneyLeft", handler.GetRemainingMoney)
+	r.Get("/{id}/transactions", handler.GetTransactions)
+	r.Get("/{id}/categories", handler.GetCategories)
+
+    r.Post("/{id}/saveInfo", handler.SaveUserInfo)
+    r.Post("/{id}/editTransaction", handler.EditTransaction)
+    r.Post("/{id}/addTransaction", handler.AddTransaction)
+    r.Post("/{id}/newCategory", handler.SaveNewCategory)
+	
+    r.Delete("/{id}/transaction", handler.DeleteTransaction)
+	r.Delete("/{id}/category", handler.DeleteCategory)
+
+	return r
 }
